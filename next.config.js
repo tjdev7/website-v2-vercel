602d485b4ef50d74524633e1495482b6aa4f9afd
Mario Jimenez <Trimmask@protonmail.com>
2025-07-02 11:39:44 -0500

    test-branch
    - Added new commands for gh-pages deployment inside the repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubPages = process.env.DEPLOY_TARGET === 'GH_PAGES';
+
+const nextConfig = {
+  distDir: "build",
+};
+
 module.exports = {
+  nextConfig,
   reactStrictMode: true,
-  // exportPathMap: () => {}
+  exportPathMap: () => {},
   swcMinify: true,
+  output: 'export',
+  assetPrefix: isGithubPages ? '/website-v2-vercel/' : '',
+  basePath: isGithubPages ? '/website-v2-vercel' : '',
 }
